fb9958cda853f2d98aef16505afa5c263faa3afe
Mehmet Özer <115498182+ozermehmett@users.noreply.github.com>
2024-02-15 18:32:55 +0300

    Initial commit

---
+++ qr_code_generator.py
@@ -0,0 +1,19 @@
+import qrcode
+
+
+def generateQRCode(file_name, url):
+    path = "qrcodes/" + file_name + ".png"
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(url)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(path)
+
+    return path
+
---
+++ telegram_bot.py
@@ -0,0 +1,29 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import qr_code_generator
+
+TOKEN = 'YOUR TOKEN HERE'
+
+
+def send_img(update, context):
+    message = update.message.text
+
+    path = qr_code_generator.generateQRCode("exam", message)
+    context.bot.send_document(chat_id=update.effective_chat.id, document=open(path, 'rb'))
+
+
+def start(update, context):
+    user_name = update.message.from_user.first_name
+    welcome_message = f"Welcome, {user_name}! This bot is a QR code generator."
+    welcome_message += "Just send a link, and it will create a QR code for you."
+    context.bot.send_message(chat_id=update.effective_chat.id, text=welcome_message)
+
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(MessageHandler(Filters.text, send_img))
+
+    updater.start_polling()
+    updater.idle()
